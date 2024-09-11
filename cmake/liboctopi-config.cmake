aa897848f1f6ab0ea8124f25c303661e2f06c1c1
Alexandre Arnt <qtgzmanager@gmail.com>
2024-09-11 16:54:25 -0300

    Added new .cmake files

--- cmake/liboctopi-config.cmake
+++
@@ -1,7 +0,0 @@
-
-include(CMakeFindDependencyMacro)
-set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR};${CMAKE_MODULE_PATH}")
-find_dependency(glib-2.0)
-find_dependency(gio-2.0)
-find_dependency(alpm)
-include("${CMAKE_CURRENT_LIST_DIR}/libalpm_octopi_utils_target.cmake")
